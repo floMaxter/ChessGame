18c802188bac0f5f929361f65a39d92dc597396e
Max Vdovin <maxpostupaet@gmail.com>
2023-03-07 22:01:03 +0300

    Created board and plugins for board.

---
+++ src/main/java/com/suai/chess/model/board/Board.java
@@ -0,0 +1,190 @@
+package com.suai.chess.model.board;
+
+import com.suai.chess.model.pieces.Alliance;
+import com.suai.chess.model.pieces.Pawn;
+import com.suai.chess.model.pieces.Piece;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Board {
+    private final List<Tile> gameBoard;
+    private final Collection<Piece> whitePieces;
+    private final Collection<Piece> blackPieces;
+
+    private final WhitePlayer whitePlayer;
+    private final BlackPlayer blackPlayer;
+    private final Player currentPlayer;
+    private final Pawn enPassantPawn;
+
+    private Board(Builder builder) {
+        this.gameBoard = createGameBoard(builder);
+        this.whitePieces = calculateActivePieces(this.gameBoard, Alliance.WHITE);
+        this.blackPieces = calculateActivePieces(this.gameBoard, Alliance.BLACK);
+        this.enPassantPawn = builder.enPassantPawn;
+
+        Collection<Move> whiteStandardMoves = calculateLegalMoves(this.whitePieces);
+        Collection<Move> blackStandardMoves = calculateLegalMoves(this.blackPieces);
+
+        this.whitePlayer = new WhitePlayer(this, whiteStandardMoves, blackStandardMoves);
+        this.blackPlayer = new BlackPlayer(this, whiteStandardMoves, blackStandardMoves);
+        this.currentPlayer = builder.nextMoveMaker.choosePlayer(this.whitePlayer, this.blackPlayer);
+    }
+
+    public Collection<Piece> getBlackPieces() {
+        return this.blackPieces;
+    }
+
+    public Collection<Piece> getWhitePieces() {
+        return this.whitePieces;
+    }
+
+    public Pawn getEnPassantPawn() {
+        return this.enPassantPawn;
+    }
+
+    public Player whitePlayer() {
+        return this.whitePlayer;
+    }
+
+    public Player blackPlayer() {
+        return this.blackPlayer;
+    }
+
+    public Player getCurrentPlayer() {
+        return this.currentPlayer;
+    }
+
+    public Piece getPiece(int coordinate) {
+        return this.gameBoard.get(coordinate).getPiece();
+    }
+
+    public Collection<Piece> getAllPieces() {
+        Collection<Piece> allPieces = new ArrayList<>();
+        allPieces.addAll(whitePieces);
+        allPieces.addAll(blackPieces);
+        return allPieces;
+    }
+
+    private Collection<Move> calculateLegalMoves(Collection<Piece> pieces) {
+        List<Move> legalMoves = new ArrayList<>();
+        for (Piece piece : pieces) {
+            legalMoves.addAll(piece.calculateLegalMoves(this));
+        }
+        return Collections.unmodifiableList(legalMoves);
+    }
+
+    private static Collection<Piece> calculateActivePieces(List<Tile> gameBoard, Alliance alliance) {
+        List<Piece> activePieces = new ArrayList<>();
+        for (Tile tile : gameBoard) {
+            if (tile.isTileOccupied()) {
+                Piece piece = tile.getPiece();
+                if (piece.getPieceAlliance() == alliance) {
+                    activePieces.add(piece);
+                }
+            }
+        }
+        return Collections.unmodifiableList(activePieces);
+    }
+
+    public Tile getTile(int tileCoordinate) {
+        return gameBoard.get(tileCoordinate);
+    }
+
+    private static List<Tile> createGameBoard(Builder builder) {
+        List<Tile> tiles = new ArrayList<>();
+        for (int i = 0; i < BoardPlugins.NUM_TILES; i++) {
+            Tile currTile = Tile.createTile(i, builder.boardConfiguration.get(i));
+            tiles.add(i, currTile);
+        }
+        return Collections.unmodifiableList(tiles);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < BoardPlugins.NUM_TILES; i++) {
+            String tileText = this.gameBoard.get(i).toString();
+            sb.append(String.format("%3s", tileText));
+            if (((i + 1) % BoardPlugins.NUM_TILES_PER_COLUMN_OR_ROW) == 0) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static Board createStandardBoard() {
+        Builder builder = new Builder();
+        builder.setPiece(new Rook(Alliance.BLACK, 0));
+        builder.setPiece(new Knight(Alliance.BLACK, 1));
+        builder.setPiece(new Bishop(Alliance.BLACK, 2));
+        builder.setPiece(new Queen(Alliance.BLACK, 3));
+        builder.setPiece(new King(Alliance.BLACK, 4, true, true));
+        builder.setPiece(new Bishop(Alliance.BLACK, 5));
+        builder.setPiece(new Knight(Alliance.BLACK, 6));
+        builder.setPiece(new Rook(Alliance.BLACK, 7));
+        builder.setPiece(new Pawn(Alliance.BLACK, 8));
+        builder.setPiece(new Pawn(Alliance.BLACK, 9));
+        builder.setPiece(new Pawn(Alliance.BLACK, 10));
+        builder.setPiece(new Pawn(Alliance.BLACK, 11));
+        builder.setPiece(new Pawn(Alliance.BLACK, 12));
+        builder.setPiece(new Pawn(Alliance.BLACK, 13));
+        builder.setPiece(new Pawn(Alliance.BLACK, 14));
+        builder.setPiece(new Pawn(Alliance.BLACK, 15));
+
+        builder.setPiece(new Pawn(Alliance.WHITE, 48));
+        builder.setPiece(new Pawn(Alliance.WHITE, 49));
+        builder.setPiece(new Pawn(Alliance.WHITE, 50));
+        builder.setPiece(new Pawn(Alliance.WHITE, 51));
+        builder.setPiece(new Pawn(Alliance.WHITE, 52));
+        builder.setPiece(new Pawn(Alliance.WHITE, 53));
+        builder.setPiece(new Pawn(Alliance.WHITE, 54));
+        builder.setPiece(new Pawn(Alliance.WHITE, 55));
+        builder.setPiece(new Rook(Alliance.WHITE, 56));
+        builder.setPiece(new Knight(Alliance.WHITE, 57));
+        builder.setPiece(new Bishop(Alliance.WHITE, 58));
+        builder.setPiece(new Queen(Alliance.WHITE, 59));
+        builder.setPiece(new King(Alliance.WHITE, 60, true, true));
+        builder.setPiece(new Bishop(Alliance.WHITE, 61));
+        builder.setPiece(new Knight(Alliance.WHITE, 62));
+        builder.setPiece(new Rook(Alliance.WHITE, 63));
+
+        builder.setMoveMaker(Alliance.WHITE);
+        return builder.build();
+    }
+
+    public Iterable<Move> getAllLegalMoves() {
+        List<Move> allLegalMoves = new ArrayList<>();
+        allLegalMoves.addAll(this.whitePlayer.getLegalMoves());
+        allLegalMoves.addAll(this.blackPlayer.getLegalMoves());
+        return Collections.unmodifiableList(allLegalMoves);
+    }
+
+    public static class Builder {
+        Map<Integer, Piece> boardConfiguration;
+        Alliance nextMoveMaker;
+        Pawn enPassantPawn;
+
+        public Builder() {
+            this.boardConfiguration = new HashMap<>();
+        }
+
+        public Builder setPiece(Piece piece) {
+            this.boardConfiguration.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public void setMoveMaker(Alliance nextMoveMaker) {
+            this.nextMoveMaker = nextMoveMaker;
+        }
+
+        public Board build() {
+            return new Board(this);
+        }
+
+        public void setEnPassantPawn(Pawn enPassantPawn) {
+            this.enPassantPawn = enPassantPawn;
+        }
+    }
+}
---
+++ src/main/java/com/suai/chess/model/board/BoardPlugins.java
@@ -0,0 +1,98 @@
+package com.suai.chess.model.board;
+
+public class BoardPlugins {
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+
+    public static final boolean[] EIGHTH_ROW_FROM_BELOW = initRow(0);
+    public static final boolean[] SEVENTH_ROW_FROM_BELOW = initRow(8);
+    public static final boolean[] SIXTH_ROW_FROM_BELOW = initRow(16);
+    public static final boolean[] FIFTH_ROW_FROM_BELOW = initRow(24);
+    public static final boolean[] FOURTH_ROW_FROM_BELOW = initRow(32);
+    public static final boolean[] THIRD_ROW_FROM_BELOW = initRow(40);
+    public static final boolean[] SECOND_ROW_FROM_BELOW = initRow(48);
+    public static final boolean[] FIRST_ROW_FROM_BELOW = initRow(56);
+
+
+    public static final String[] ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
+    public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+
+
+    public static final int NUM_TILES = 64;
+    public static final int NUM_TILES_PER_COLUMN_OR_ROW = 8;
+
+    private BoardPlugins() {
+        throw new RuntimeException("It is impossible to create such a board!");
+    }
+
+    private static String[] initializeAlgebraicNotation() {
+        /*"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+         "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+         "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+         "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+         "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+         "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+         "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+         "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"));*/
+
+        String[] finalArray = new String[64];
+        int[] numbers = {8, 7, 6, 5, 4, 3, 2, 1};
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        int count = 0;
+        for(int x : numbers) {
+            for (String y : letters) {
+                finalArray[count] = y + x;
+                count++;
+            }
+        }
+        return finalArray;
+    }
+
+    private static Map<String, Integer> initializePositionToCoordinateMap() {
+        Map<String, Integer> positionToCoordinate = new HashMap<>();
+        for (int i = 0; i < NUM_TILES; i++) {
+            positionToCoordinate.put(ALGEBRAIC_NOTATION[i], i);
+        }
+        return Collections.unmodifiableMap(positionToCoordinate);
+    }
+
+    private static boolean[] initColumn(int columnNumber) {
+        boolean[] column = new boolean[NUM_TILES];
+        for (int i = 0; i < NUM_TILES_PER_COLUMN_OR_ROW; i++)
+            column[i * NUM_TILES_PER_COLUMN_OR_ROW + columnNumber] = true;
+        return column;
+    }
+
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        do {
+            row[rowNumber] = true;
+            rowNumber++;
+        } while ((rowNumber % NUM_TILES_PER_COLUMN_OR_ROW) != 0);
+        return row;
+    }
+
+    public static boolean isValidTileCoordinate(int coordinate) {
+        return coordinate >= 0 && coordinate < NUM_TILES;
+    }
+
+    public static int getCoordinateAtPosition(String position) {
+        return POSITION_TO_COORDINATE.get(position);
+    }
+    public static String getPositionAtCoordinate(int destinationCoordinate) {
+        return ALGEBRAIC_NOTATION[destinationCoordinate];
+    }
+
+    public static boolean isEndGame(Board board) {
+        return board.getCurrentPlayer().isInCheckMate() ||
+                board.getCurrentPlayer().getOpponent().isInCheckMate();
+    }
+
+    public static boolean isCheckSituation(Board board) {
+        return board.getCurrentPlayer().isInCheck() ||
+                board.getCurrentPlayer().getOpponent().isInCheck();
+    }
+}
